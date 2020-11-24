5369a8746b64237da849e706d85411cf130d7868
Eb Peña <eberg.pena@gmail.com>
2020-11-23 22:24:15 -0600

    Unaclase base puere hederar de una clase padre

--- Clases & Objetos.playground/Contents.swift
+++ Clases & Objetos.playground/Contents.swift
@@ -1,6 +1,6 @@
 import UIKit
 
-class automovil {
+class claseautomovil {
     
     var color = "neutro"
     var numeroLlantas = 4
@@ -19,11 +19,50 @@ class automovil {
     }
 }
 
-var miObjetoMazda = automovil()
+var miObjetoMazda = claseautomovil()
 
 
 
 miObjetoMazda.precio
-
 miObjetoMazda.precio = 5000
+
 print(miObjetoMazda.precio)
+
+
+struct estructuraAutomovil {
+    var color = "neutro"
+    var numeroLlantas = 4
+    var precio = 0
+    
+    func enceder() -> Bool {
+        return true
+    }
+    
+    func apagar() -> Bool {
+        return true
+    }
+    
+    func acelerar() -> Bool {
+        return true
+    }
+}
+
+var miEstructuraMazda = estructuraAutomovil ()
+
+miEstructuraMazda.precio
+miEstructuraMazda.precio = 5000
+
+print(miEstructuraMazda.precio)
+
+
+var miSegundoObjetoMazda = miObjetoMazda
+var miSegundaEstructuraMazda = miEstructuraMazda
+miSegundoObjetoMazda.precio = 6000
+miSegundaEstructuraMazda.precio = 8100
+
+//Tiene su misma estructura de memoria 
+print(miSegundoObjetoMazda.precio)
+print(miSegundaEstructuraMazda.precio)
+
+print(miObjetoMazda.precio)
+print(miEstructuraMazda.precio)
